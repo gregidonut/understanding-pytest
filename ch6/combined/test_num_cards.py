f2c7f9d71a8ab2d0d41d0988642eef62286b8dea
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-09 11:38:58 +0800

    test: add more explicit test cases with the usage of num_cards mark
    Here we check the outputs of no mark vs num_cards mark with no params
    and using the mark with 3 and 10 as params

--- ch6/combined/test_num_cards.py
+++ ch6/combined/test_num_cards.py
@@ -1,8 +1,24 @@
 import pytest
 
-NUM_CARDS = 4
+
+def test_no_marker(cards_db):
+    assert cards_db.count() == 0
 
 
-@pytest.mark.num_cards(NUM_CARDS)
-def test_num_cards(cards_db):
-    assert cards_db.count() == NUM_CARDS
+@pytest.mark.num_cards
+def test_marker_with_no_param(cards_db):
+    assert cards_db.count() == 0
+
+
+@pytest.mark.num_cards(3)
+def test_three_cards(cards_db):
+    assert cards_db.count() == 3
+    # just for fun, let's look at the cards Faker made for us
+    print()
+    for c in cards_db.list_cards():
+        print(c)
+
+
+@pytest.mark.num_cards(10)
+def test_ten_cards(cards_db):
+    assert cards_db.count() == 10
