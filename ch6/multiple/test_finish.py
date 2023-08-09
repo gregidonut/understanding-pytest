de873b6bff103418af9279b7cbbd3127346845af
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-09 10:11:59 +0800

    test: show how you can mark a file, a class or a param
    Doing this you can see how there might be use cases in doing it a couple
    of different ways.

---
+++ ch6/multiple/test_finish.py
@@ -0,0 +1,60 @@
+import pytest
+from cards import Card, InvalidCardId
+
+pytestmark = pytest.mark.finish
+
+
+@pytest.mark.smoke
+class TestFinish:
+    def test_finish_from_todo(self, cards_db):
+        i = cards_db.add_card(
+            Card(
+                "foo",
+                state="todo",
+            )
+        )
+        cards_db.finish(i)
+        c = cards_db.get_card(i)
+        assert c.state == "done"
+
+    def test_finish_from_in_prog(self, cards_db):
+        i = cards_db.add_card(
+            Card(
+                "foo",
+                state="in prog",
+            )
+        )
+        cards_db.finish(i)
+        c = cards_db.get_card(i)
+        assert c.state == "done"
+
+    def test_finish_from_done(self, cards_db):
+        i = cards_db.add_card(
+            Card(
+                "foo",
+                state="done",
+            )
+        )
+        cards_db.finish(i)
+        c = cards_db.get_card(i)
+        assert c.state == "done"
+
+
+@pytest.mark.parametrize(
+    "start_state",
+    [
+        "todo",
+        pytest.param("in prog", marks=pytest.mark.smoke),
+        "done",
+    ]
+)
+def test_finish_func(cards_db, start_state):
+    i = cards_db.add_card(
+        Card(
+            "foo",
+            state=start_state,
+        )
+    )
+    cards_db.finish(i)
+    c = cards_db.get_card(i)
+    assert c.state == "done"
